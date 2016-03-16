0fc8abf4977005a0cd358b3fdd8bba95633a027c
James Lipe <james@jameslipe.com>
2016-03-16 09:16:54 -0400

    Added colour and styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313152354) do
+ActiveRecord::Schema.define(version: 20160314012939) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160313152354) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
   end
 
 end
