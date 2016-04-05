0dc001334986a137d4f172f51569e1993dbeeaf9
James Lipe <james@jameslipe.com>
2016-04-04 23:59:43 -0400

    5.1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,6 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
-    @test_variable = 422
   end
 
   # GET /products/1
@@ -29,7 +28,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to "static_pages/landing_page" , notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :products
-  resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'
 
