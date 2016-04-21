6e409e66a8738b934af523c869fd3fb486eb09e6
James Lipe <james@jameslipe.com>
2016-04-21 15:00:13 -0400

    5.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,15 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(3)
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(from: @email,
+  		to: "james@jameslipe.com",
+  		subject: "A new message from #{@name}",
+  		body: @message).deliver_now
+  end
+
 end
