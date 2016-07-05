176aa1ed969d3787a9d81a8cf78397753e292107
James Lipe <james@jameslipe.com>
2016-07-05 17:02:02 -0400

    6.14 Caching

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'will_paginate', '~> 3.1.0'
 gem 'stripe'
 #Angular JS
 gem 'angularjs-rails'
+#Dalli
+gem 'dalli'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -21,7 +21,6 @@ app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
 
   $scope.addOrder = function(){
     if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
-    // $scope.newOrder.id = $scope.orders[$scope.orders.length - 1].id + 1;
     order = models.orders.save($scope.newOrder, function(){
       recent_order = models.orders.get({id: order.id});
       $scope.orders.push(recent_order);
